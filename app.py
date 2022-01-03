613bee1b4c9403e4d02695ddd594429caf3decf7
Mohd Hafiz <97020430+mhafiznj@users.noreply.github.com>
2022-01-03 16:50:09 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+
+
+@app.route("/",methods=["GET"])
+def index_get():
+    return render_template("base.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    text = request.get_json().get("message")
+    # TODO: check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
